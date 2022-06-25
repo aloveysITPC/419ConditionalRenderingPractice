2ce2292a044bbf84850803927d51bd584623f0b2
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-06-25 07:00:24 -0400

    finetuning

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,8 @@ import Input from "./Input";
 function Form(props) {
   return (
     <form className="form">
-      <Input type="text" placeholder="password" />
+      <Input type="text" placeholder="UserName" />
+      <Input type="password" placeholder="Password" />
       <button type="submit">Register</button>
     </form>
   );
