b82cffda24b179ee24b7a63e1b2491a045e0a106
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-06-25 07:05:16 -0400

    fineTune

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,6 +6,7 @@ function Form(props) {
     <form className="form">
       <Input type="text" placeholder="UserName" />
       <Input type="password" placeholder="Password" />
+      <Input type="password" placeholder="Confirm Password" />
       <button type="submit">Register</button>
     </form>
   );
